86aacb4658752615e46c29ca81388e6a81897ad4
Bilalmemon041 <Bilalmemon041@gmail.com>
2024-10-15 02:32:07 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var form = document.getElementById('resume-form');
+var resumeDisplay = document.getElementById('display');
+// Handle form submission
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    // Collect input values
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var phone = document.getElementById('phone').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skill = document.getElementById('skill').value;
+    // Generate the resume HTML
+    var resumeHTML = "\n    <h2><b>Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b> ".concat(name, "</p>\n    <p><b>Email:</b> ").concat(email, "</p>\n    <p><b>Phone:</b> ").concat(phone, "</p>\n\n    <h3>Education</h3>\n    <p>").concat(education, "</p>\n\n    <h3>Experience</h3>\n    <p>").concat(experience, "</p>\n\n    <h3>Skills</h3>\n    <p>").concat(skill, "</p>\n    ");
+    // Display the resume content
+    if (resumeDisplay) {
+        resumeDisplay.innerHTML = resumeHTML;
+    }
+    else {
+        console.error('The resume display element is missing');
+    }
+});
---
+++ script.ts
@@ -0,0 +1,40 @@
+const form = document.getElementById('resume-form') as HTMLFormElement;
+const resumeDisplay = document.getElementById('display') as HTMLDivElement;
+
+// Handle form submission
+form.addEventListener('submit', (event: Event) => {
+    event.preventDefault();
+
+    // Collect input values
+    const name = (document.getElementById('name') as HTMLInputElement).value;
+    const email = (document.getElementById('email') as HTMLInputElement).value;
+    const phone = (document.getElementById('phone') as HTMLInputElement).value;
+    const education = (document.getElementById('education') as HTMLTextAreaElement).value;
+    const experience = (document.getElementById('experience') as HTMLTextAreaElement).value;
+    const skill = (document.getElementById('skill') as HTMLTextAreaElement).value;
+
+    // Generate the resume HTML
+    const resumeHTML = `
+    <h2><b>Resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b> ${name}</p>
+    <p><b>Email:</b> ${email}</p>
+    <p><b>Phone:</b> ${phone}</p>
+
+    <h3>Education</h3>
+    <p>${education}</p>
+
+    <h3>Experience</h3>
+    <p>${experience}</p>
+
+    <h3>Skills</h3>
+    <p>${skill}</p>
+    `;
+
+    // Display the resume content
+    if (resumeDisplay) {
+        resumeDisplay.innerHTML = resumeHTML;
+    } else {
+        console.error('The resume display element is missing');
+    }
+});
